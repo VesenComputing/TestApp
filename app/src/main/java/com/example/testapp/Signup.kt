87383e170aeca22341a28ead45b7d97eadeef640
aileentoo <aileenchero11@gmail.com>
2020-07-02 21:22:53 +0300

    updata i the signup

--- app/src/main/java/com/example/testapp/Signup.kt
+++ app/src/main/java/com/example/testapp/Signup.kt
@@ -4,57 +4,72 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_signup.*
 import kotlin.math.sign
 import kotlin.time.measureTimedValue
 
-class Signup : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+
+class Signup<Menu> : AppCompatActivity() {
+    val mAuth = FirebaseAuth.getInstance()
+    lateinit var mDatabase: DatabaseReference
+
+    interface DatabaseReference {
+
+    }
+
+    var user = FirebaseAuth.getInstance().currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        val signup=findViewById<View>(R.id.signup_btn) as Button
+       // mDatabase=FirebaseDatabase.getInstance().getReference("Name")
 
-        auth = FirebaseAuth.getInstance();
+        mDatabase = Firebase.database.reference
+        signup_btn.setOnClickListener(View.OnClickListener {
+            signupUser()
+        })
 
-        signup_btn.setOnClickListener {
-
-           signUpUser()
-
-        }
     }
-   private fun signUpUser() {
-       if (Email1.text.toString().isEmpty()) {
-           Email1.error = "Please enter Email"
-           username.requestFocus()
-           return
+    private fun signupUser(){
+        val email1= findViewById<View>(R.id.Email1) as EditText
+        val password1 = findViewById<View>(R.id.Password1) as EditText
+        val usernameTxt= findViewById<View>(R.id.username) as EditText
 
-       }
-       if (!Patterns.EMAIL_ADDRESS.matcher(Email1.text.toString()).matches()) {
-           Email1.error = "Please enter valid Email"
-           Email1.requestFocus()
-           return
 
-       }
-       if (Password1.text.toString().isEmpty()) {
-           Password1.error = "Please enter Password"
-           username.requestFocus()
-           return
-       }
+        val email = email1.text.toString()
+        val password =password1.text.toString()
+        val username =username.text.toString()
+        if (!username.isEmpty() && !password.isEmpty() && !email.isEmpty()){
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this,
+                OnCompleteListener {task->
+                    if(task.isSuccessful){
+                        val user = mAuth.currentUser
+                        val uid= user
 
-       auth.signInWithEmailAndPassword(Email1.text.toString(), Password1.text.toString())
-           .addOnCompleteListener(this) { task ->
-               if (task.isSuccessful) {
-                   val user = auth.currentUser
-                   startActivity(Intent(this, Home::class.java))
-                   Toast.makeText(this,"Login successful",Toast.LENGTH_SHORT).show()
-               } else {
-                   Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT
-                   ).show()
-               }
+                    }else{
+                        Toast.makeText(this,"Error:(",Toast.LENGTH_LONG).show()
+                    }
+                })
 
-           }
-   }
+        }else{
+            Toast.makeText(this, "Please enter creditional:(",Toast.LENGTH_SHORT).show()
+        }
 
+    }
 }
+
+
+
+
